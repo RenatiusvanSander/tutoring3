2ffd1218e8898182676d8288932ef1883ff7949f
Remy Meier <remad@web.de>
2025-08-02 18:28:31 +0200

    feature/oauth2resourceserver: Configurations are now part of RootConfigClasses

--- src/main/java/edu/remad/tutoring3/config/SpringSecurityConfig.java
+++ src/main/java/edu/remad/tutoring3/config/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package edu.remad.tutoring3.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SpringSecurityConfig {
 
 	@Bean
--- src/main/java/edu/remad/tutoring3/webappinitializing/Tutoring3Appinitializer.java
+++ src/main/java/edu/remad/tutoring3/webappinitializing/Tutoring3Appinitializer.java
@@ -1,25 +1,43 @@
 package edu.remad.tutoring3.webappinitializing;
 
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import edu.remad.tutoring3.config.FreeMarkerConfig;
+import edu.remad.tutoring3.config.SpringSecurityConfig;
 import edu.remad.tutoring3.config.TutoringAppConfig;
+import edu.remad.tutoring3.config.VelocityConfig;
 
 public class Tutoring3Appinitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		return new Class[0];
+		return new Class[] { TutoringAppConfig.class, FreeMarkerConfig.class, VelocityConfig.class, SpringSecurityConfig.class };
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		return new Class[] { TutoringAppConfig.class, FreeMarkerConfig.class };
+		return null;
 	}
 
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
+	}
+	
+    @Override
+	protected WebApplicationContext createRootApplicationContext() {
+		Class<?>[] configClasses = getRootConfigClasses();
+		if (!ObjectUtils.isEmpty(configClasses)) {
+			AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+			context.register(configClasses);
+
+			return context;
+		} else {
+			return null;
+		}
 	}
 
 }
