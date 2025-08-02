1f0845c38540f400cb29d687553a4593981cbabe
Remy Meier <remad@web.de>
2025-08-02 13:11:19 +0200

    feature/oauth2resourceserver: initial setup

---
+++ src/main/java/edu/remad/tutoring3/config/SpringSecurityConfig.java
@@ -0,0 +1,29 @@
+package edu.remad.tutoring3.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.oauth2ResourceServer(c -> c.jwt(j -> j.jwkSetUri("https://keycloak.local:8443/realms/ConnectTrial/protocol/openid-connect/certs")));
+
+		http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
+
+		return http.build();
+	}
+
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		return NimbusJwtDecoder.withJwkSetUri("https://keycloak.local:8443/realms/ConnectTrial/protocol/openid-connect/certs").build();
+	}
+}
