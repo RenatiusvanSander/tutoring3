f253b8fb8d607eaeb9c8872023e95511f2fc36e7
Remy Meier <remad@web.de>
2025-11-23 19:12:57 +0100

    feature/tutoring-appointment: tutoring appointment service, repository and controller

---
+++ src/main/java/edu/remad/tutoring3/controllers/ApiTutoringAppointmentController.java
@@ -0,0 +1,142 @@
+package edu.remad.tutoring3.controllers;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.remad.tutoring3.dto.TutoringAppointmentDto;
+import edu.remad.tutoring3.helper.LocalDateTimeHelper;
+import edu.remad.tutoring3.persistence.models.ServiceContractEntity;
+import edu.remad.tutoring3.persistence.models.TutoringAppointmentEntity;
+import edu.remad.tutoring3.persistence.models.UserEntity;
+import edu.remad.tutoring3.services.ServiceContractService;
+import edu.remad.tutoring3.services.TutoringAppointmentEntityService;
+import edu.remad.tutoring3.services.UserEntityService;
+
+/**
+ * Controls API REST Endpoints for tutoring appointments'
+ * 
+ * @author edu.remad
+ * @since 2025
+ */
+@RequestMapping("/api/tutoring-appointments")
+@RestController
+public class ApiTutoringAppointmentController {
+
+	private final TutoringAppointmentEntityService appointmentService;
+
+	private final UserEntityService userService;
+	
+	private final ServiceContractService serviceContractService;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param appointmentEntityService {@link TutoringAppointmentEntityService}
+	 * @param userEntityService        {@link UserEntityService}
+	 * @param serviceContractService {@link ServiceContractService}
+	 */
+	public ApiTutoringAppointmentController(TutoringAppointmentEntityService appointmentEntityService,
+			UserEntityService userEntityService, ServiceContractService serviceContractService) {
+		appointmentService = appointmentEntityService;
+		userService = userEntityService;
+		this.serviceContractService = serviceContractService;
+	}
+
+	/**
+	 * Saves a tutoring appointment
+	 * 
+	 * @param tutoringAppointmentDto {@link TutoringAppointmentDto}
+	 * @return json-encoded {@link TutoringAppointmentDto}
+	 */
+	@PostMapping("/save")
+	public ResponseEntity<TutoringAppointmentDto> saveTutoringAppointment(
+			@RequestBody TutoringAppointmentDto tutoringAppointmentDto) {
+		UserEntity loadedUser = userService.getReferencedUserEntityById(tutoringAppointmentDto.getTutoringAppointmentUser());
+		ServiceContractEntity loadeServiceContract = serviceContractService.getReferencedServiceContractById(tutoringAppointmentDto.getServiceContractId());
+		TutoringAppointmentEntity newAppointment = new TutoringAppointmentEntity(tutoringAppointmentDto, loadedUser, loadeServiceContract);
+		newAppointment.setTutoringAppointmentCreationDate(LocalDateTime.now());
+		TutoringAppointmentEntity savedAppointment = appointmentService.saveTutoringApointment(newAppointment);
+
+		return new ResponseEntity<>(new TutoringAppointmentDto(savedAppointment), HttpStatusCode.valueOf(201));
+	}
+
+	/**
+	 * Loads a tutoring appointment
+	 * 
+	 * @param id Tutoring Appointment's identifier
+	 * @return json-encoded loaded {@link TutoringAppointmentDto}
+	 */
+	@GetMapping("/get/{id}")
+	public ResponseEntity<TutoringAppointmentDto> loadTutoringAppointment(@PathVariable("id") Long id) {
+		TutoringAppointmentEntity loadedAppointment = appointmentService.loadTutoringApointment(id);
+
+		return new ResponseEntity<TutoringAppointmentDto>(new TutoringAppointmentDto(loadedAppointment),
+				HttpStatusCode.valueOf(200));
+	}
+	
+	/**
+	 * Gets tutoring appointments of user
+	 * 
+	 * @param userId user's identifier
+	 * @return json-encoded {@link TutoringAppointmentDto}
+	 */
+	@GetMapping("/get/by-user-id/{userId}")
+	public ResponseEntity<List<TutoringAppointmentDto>> getTutoringAppointmentsByUserId(@PathVariable("userId") Long userId) {
+		List<TutoringAppointmentEntity> loadedAppointments = appointmentService.loadTutoringApointmentByUserId(userId);
+		List<TutoringAppointmentDto> appointments = loadedAppointments.stream().map(TutoringAppointmentDto::new).collect(Collectors.toList());
+		
+		return new ResponseEntity<>(appointments, HttpStatusCode.valueOf(200));
+	}
+	
+	/**
+	 * Gets service contracts by ids
+	 * 
+	 * @param ids tutoring appointments' identifiers
+	 * @return json-encoded {@link TutoringAppointmentDto}
+	 */
+	@GetMapping("/get/by-ids")
+	public ResponseEntity<List<TutoringAppointmentDto>> getTutoringAppointmentByIds(@RequestParam(value = "id") List<Long> ids) {
+		List<TutoringAppointmentEntity> loadedTutoringAppointments = appointmentService.loadTutoringApointmentByIds(ids);
+		List<TutoringAppointmentDto> tutoringAppointments = loadedTutoringAppointments.stream()
+				.map(TutoringAppointmentDto::new).collect(Collectors.toList());
+		
+		return new ResponseEntity<>(tutoringAppointments, HttpStatusCode.valueOf(200));
+	}
+	
+	/**
+	 * Updates single appointment
+	 * 
+	 * @param tutoringAppointmentDto {@link TutoringAppointmentDto}
+	 * @return json-encoded {@link TutoringAppointmentDto}
+	 */
+	@PutMapping("/update/single-appointment")
+	public ResponseEntity<TutoringAppointmentDto> updateSingleTutoringAppointment(@RequestBody TutoringAppointmentDto tutoringAppointmentDto) {
+		TutoringAppointmentEntity loadedAppointment = appointmentService.loadTutoringApointment(tutoringAppointmentDto.getTutoringAppointmentNo());
+		ServiceContractEntity loadedServiceContract = serviceContractService.findServiceContractById(tutoringAppointmentDto.getServiceContractId());
+		
+		if(loadedAppointment != null && loadedServiceContract != null) {
+			loadedAppointment.setServiceContractId(loadedServiceContract);
+			loadedAppointment.setAccomplished(tutoringAppointmentDto.isAccomplished());
+			loadedAppointment.setTutoringAppointmentDate(LocalDateTimeHelper.convertIsoTimeStringToLocalDateTime(tutoringAppointmentDto.getTutoringAppointmentDate()));
+			loadedAppointment.setTutoringAppointmentStartDateTime(LocalDateTimeHelper.convertIsoTimeStringToLocalDateTime(tutoringAppointmentDto.getTutoringAppointmentStartDateTime()));
+			loadedAppointment.setTutoringAppointmentEndDateTime(LocalDateTimeHelper.convertIsoTimeStringToLocalDateTime(tutoringAppointmentDto.getTutoringAppointmentEndDateTime()));
+			
+			loadedAppointment = appointmentService.updateSingleTutoringAppointment(loadedAppointment);
+		}
+		
+		return new ResponseEntity<>(new TutoringAppointmentDto(loadedAppointment), HttpStatusCode.valueOf(200));
+	}
+	
+}
---
+++ src/main/java/edu/remad/tutoring3/services/impl/TutoringAppointmentEntityServiceImpl.java
@@ -0,0 +1,56 @@
+package edu.remad.tutoring3.services.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import edu.remad.tutoring3.persistence.models.TutoringAppointmentEntity;
+import edu.remad.tutoring3.repositories.TutoringAppointmentEntityRepository;
+import edu.remad.tutoring3.services.TutoringAppointmentEntityService;
+
+/**
+ * Serves to load, save, get, put, patch, delete a tutoring appointment
+ * 
+ * @author edu.remad
+ * @since 2025
+ */
+@Service
+public class TutoringAppointmentEntityServiceImpl implements TutoringAppointmentEntityService {
+
+	private final TutoringAppointmentEntityRepository tutoringAppointmentEntityRepository;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param appointmentRepository {@link TutoringAppointmentEntityRepository}
+	 */
+	public TutoringAppointmentEntityServiceImpl(TutoringAppointmentEntityRepository appointmentRepository) {
+		tutoringAppointmentEntityRepository = appointmentRepository;
+	}
+	
+	@Override
+	public TutoringAppointmentEntity saveTutoringApointment(TutoringAppointmentEntity appointment) {
+		return tutoringAppointmentEntityRepository.save(appointment);
+	}
+
+	@Override
+	public TutoringAppointmentEntity loadTutoringApointment(Long id) {
+		return tutoringAppointmentEntityRepository.findById(id).get();
+	}
+
+	@Override
+	public List<TutoringAppointmentEntity> loadTutoringApointmentByIds(List<Long> ids) {
+		return tutoringAppointmentEntityRepository.findAllById(ids);
+	}
+
+	@Override
+	public List<TutoringAppointmentEntity> loadTutoringApointmentByUserId(Long userId) {
+		return tutoringAppointmentEntityRepository.findByTutoringAppointmentUser_UserId(userId);
+	}
+
+	@Override
+	public TutoringAppointmentEntity updateSingleTutoringAppointment(TutoringAppointmentEntity appointmentToUpdate) {
+		return tutoringAppointmentEntityRepository.save(appointmentToUpdate);
+	}
+
+}
