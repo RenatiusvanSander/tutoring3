9ec8f3929d0c056091544f0a0c3a760cf148590d
Remy Meier <remad@web.de>
2026-03-28 10:20:43 +0100

    feature/invoice-pdf: InvoiceEntityPdfController transfer invoice pdf bytes to SPA

--- src/main/java/edu/remad/tutoring3/controllers/ApiInvoiceEntityPdfController.java
+++ src/main/java/edu/remad/tutoring3/controllers/ApiInvoiceEntityPdfController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.remad.tutoring3.helper.DownloadHelper;
@@ -51,7 +52,7 @@ public class ApiInvoiceEntityPdfController {
 	public ResponseEntity<byte[]> getPdfInvoice(@PathVariable("id") long invoiceId) {
 		byte[] pdfInvoiceFile = invoicePdfService.createAndSaveInvoiceFile(invoiceId);
 		
-		if(pdfInvoiceFile.length == 0) {
+		if(pdfInvoiceFile == null || pdfInvoiceFile.length == 0) {
 			return ResponseEntity.notFound().build();
 		}
 		
@@ -61,8 +62,8 @@ public class ApiInvoiceEntityPdfController {
 				.headers(httpHeaders).body(pdfInvoiceFile);
 	}
 
-	@GetMapping(value = "/getPdfInvoicesInOneFile/{ids}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-	public ResponseEntity<byte[]> getPdfInvoices(@PathVariable("ids") List<Long> ids) {
+	@GetMapping(value = "/getPdfInvoicesInOneFile", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	public ResponseEntity<byte[]> getPdfInvoices(@RequestParam(value = "id") List<Long> ids) {
 		List<InvoiceEntity> invoiceEntities = invoiceEntityService.loadInvoicesByIds(ids);
 		byte[] infoicesPfs = null; // pdfCreatorService.createInvoicesPdfs(invoiceEntities);
 		String joinedIds = invoiceEntities.stream().map(String::valueOf).collect(Collectors.joining("_"));
